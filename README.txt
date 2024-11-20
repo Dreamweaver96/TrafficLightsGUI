b2a2036d7bdf6f281a0baf19254c389ff5a83a0f
Dreamweaver96 <wojciech.b96@gmail.com>
2024-11-20 23:31:42 +0100

    fixed issue with enabled pedestrian button after switching the modes and updated README.

--- README.txt
+++ README.txt
@@ -1,24 +1,45 @@
+ENG:
+Requirements:
+Create a GUI (Graphical User Interface) with:
+
+A button for the pedestrian.
+A button for the operator.
+Five lights: two for pedestrians and three for cars.
+
+
+Traffic Light Operation Sequence (Day Mode):
+1.In normal operation mode, all lights are turned off.
+2. After the pedestrian presses the button:
+2.1.The green light for cars turns on, and the red light for pedestrians turns on (for 2 seconds).
+2.2.The orange light for cars turns on (for 2 seconds).
+2.3.The red light for cars turns on (for 2 seconds).
+2.4.The green light for pedestrians turns on (for 10 seconds) simultaneously with the red light for cars.
+2.5.The red light for pedestrians turns on (for 2 seconds).
+2.6.The red and orange lights for cars turn on (for 2 seconds).
+2.7.The green light for cars turns on (for 10 seconds).
+2.8.Both the green light for cars and the red light for pedestrians turn off.
+
+Traffic Light Operation Sequence (Night Mode):
+1. In night mode, all lights are turned off except for the orange light for cars, which blinks at a frequency of 1 hertz (alternating on and off every 0.5 seconds).
+
+
+PL:
 Sterowanie światłami drogowymi (Zadanie z GUI w PYTHONIE):
 
-Wejścia do appki:
-1.Przycisk dla pieszego
-Wyjścia z appki:
-1.Światło czerwone dla pieszego
-2.Światło zielone dla pieszego
-3.Światło czerwone dla samochodu
-4.Światło pomarańczowe dla samochodu
-5.Światło zielone dla samochodu
-
-Sekwencja pracy świateł:
-1.W normalnym trybie pracy światła dla samochodu są wyłączone, co oznacza to samo, co światło zielone, dla pieszego świeci się światło czerwone
-2.Po naciśnięciu przez pieszego przycisku
-2.1.Dla samochodu załącza się światło zielone (na 2 Sekundy)
+Sekwencja pracy świateł (tryb dzienny):
+1.W normalnym trybie pracy światła wszystkie światła są wyłączone
+2.Po naciśnięciu przez pieszego przycisku 
+2.1.Dla samochodu załącza się światło zielone a dla pieszego światło czerwone (na 2 Sekundy)
 2.2.Dla samochodu załącza się światło pomarańczowe (na 2 Sekundy)
 2.3.Dla samochodu załącza się światło czerwone (świeci przez 2 Sekundy)
 2.4.Dla pieszego załącza się światło zielone (na 10 Sekund), równolegle ze światłem czerwonym dla samochodu
 2.5.Dla pieszego załącza się światło czerwone (na 2 Sekundy)
 2.6.Dla samochodu załączają się światła czerwone i pomarańczowe (na 2 Sekundy)
 2.7.Dla samochodu załącza się światło zielone (na 10 Sekund)
-2.8.Światło zielone dla samochodu gaśnie
+2.8.Światło zielone dla samochodu i światło czerwone dla pieszego gaśnie 
 
-Dorób do tego GUI (interfejs użytkownika z Przyciskiem dla pieszego oraz ze piećioma światłami - dwoma dla pieszego i trzema dla samochodu).
+Sekwencja pracy świateł (tryb nocny):
+1. W trybie nocnym wszystkie światła są wyłączone z wyjątkiem światła pomarańczowego dla samochodu, które miga z częstotliwością 1 herca (czyli naprzemiennie załącza się na pół sekundy i wyłącza się na pół sekundy).
+
+
+Dorób do tego GUI (interfejs użytkownika) z Przyciskiem dla pieszego, przyciskiem dla operatora oraz ze pięcioma światłami - dwoma dla pieszego i trzema dla samochodu).
--- main.py
+++ main.py
@@ -3,33 +3,31 @@ import tkinter
 root = tkinter.Tk()
 root.geometry('900x700')
 
-
 '''Lights being on or off are determined by fill parameters of each index number of tuples containing colours. 
 0 - light is off
 1 - light is on
 Each colour has its own two-element tuple declared below.
 '''
-
 green = ('#175700','#3DE500')
 yellow = ('#957700','#F9C700')
 red = ('#7D0000','#F90000')
 
+#1.In normal operation mode, all lights are turned off.
 TrafficSemaphor = tkinter.Canvas(height=450, width=200, bg='black')
 TrafficSemaphor.place(x=100, y=100)
-#1a.W normalnym trybie pracy światła dla samochodu są wyłączone, co oznacza to samo, co światło zielone
+
 red_traffic = TrafficSemaphor.create_oval(50, 25, 150, 125, fill=red[0])
 yellow_traffic = TrafficSemaphor.create_oval(50, 175, 150, 275, fill=yellow[0])
 green_traffic = TrafficSemaphor.create_oval(50, 325, 150, 425, fill=green[0])
 
 PedestrianSemaphor = tkinter.Canvas(height=300, width=200, bg='black')
 PedestrianSemaphor.place(x=350, y=175)
-#1b.W normalnym trybie pracy dla pieszego świeci się światło czerwone
 red_pedestrian = PedestrianSemaphor.create_oval(50, 25, 150, 125, fill=red[0])
 green_pedestrian = PedestrianSemaphor.create_oval(50, 175, 150, 275, fill=green[0])
 
 
 def switch_mode():
-    global b_switch_state
+    global b_switch_state, sequence_flag
     if b_switch_state == 'day':
         b_switch.config(text="LIGHTS MODE:\n NIGHT\n\n PRESS TO SWITCH\nTO DAY MODE", background="blue",fg="yellow")
         b_switch_state = 'night'
@@ -37,11 +35,13 @@ def switch_mode():
     else:
         b_switch.config(text="LIGHTS MODE:\n DAY\n\n PRESS TO SWITCH\nTO NIGHT MODE", background="green", fg="white")
         b_switch_state = 'day'
-        b_pedestrian.config(state="normal")
+        if sequence_flag == False:
+            b_pedestrian.config(state="normal")
 
 
-#2.Po naciśnięciu przez pieszego przycisku
-# 2.1.Dla samochodu załącza się światło zielone (na 2 Sekundy)
+#2. After the pedestrian presses the button:
+#2.1. The green light for cars turns on, and the red light for pedestrians turns on (for 2 seconds).
+
 def step_one():
     global sequence_flag
     sequence_flag = True
@@ -51,36 +51,36 @@ def step_one():
     PedestrianSemaphor.itemconfig(red_pedestrian, fill=red[1])
     root.after(2000, step_two)
 
-#2.2.Dla samochodu załącza się światło pomarańczowe (na 2 Sekundy)
+#2.2. The orange light for cars turns on (for 2 seconds)
 def step_two():
     TrafficSemaphor.itemconfig(green_traffic, fill=green[0])
     TrafficSemaphor.itemconfig(yellow_traffic, fill=yellow[1])
     root.after(2000, step_three)
 
-#2.3.Dla samochodu załącza się światło czerwone (świeci przez 2 Sekundy)
+#2.3. The red light for cars turns on (for 2 seconds).
 def step_three():
     TrafficSemaphor.itemconfig(yellow_traffic, fill=yellow[0])
     TrafficSemaphor.itemconfig(red_traffic, fill=red[1])
     root.after(2000, step_four)
 
-#2.4.Dla pieszego załącza się światło zielone (na 10 Sekund), równolegle ze światłem czerwonym dla samochodu
+#2.4. The green light for pedestrians turns on (for 10 seconds) simultaneously with the red light for cars.
 def step_four():
     PedestrianSemaphor.itemconfig(red_pedestrian, fill=red[0])
     PedestrianSemaphor.itemconfig(green_pedestrian, fill=green[1])
     root.after(10000, step_five)
 
-#2.5.Dla pieszego załącza się światło czerwone (na 2 Sekundy)
+#2.5. The red light for pedestrians turns on (for 2 seconds).
 def step_five():
     PedestrianSemaphor.itemconfig(green_pedestrian, fill=green[0])
     PedestrianSemaphor.itemconfig(red_pedestrian, fill=red[1])
     root.after(2000,step_six)
 
-#2.6.Dla samochodu załączają się światła czerwone i pomarańczowe (na 2 Sekundy)
+#2.6. The red and orange lights for cars turn on (for 2 seconds).
 def step_six():
     TrafficSemaphor.itemconfig(yellow_traffic, fill=yellow[1])
     root.after(2000, step_seven)
 
-#2.7.Dla samochodu załącza się światło zielone (na 10 Sekund)
+#2.7. The green light for cars turns on (for 10 seconds).
 def step_seven():
     TrafficSemaphor.itemconfig(yellow_traffic, fill=yellow[0])
     TrafficSemaphor.itemconfig(red_traffic, fill=red[0])
@@ -88,13 +88,16 @@ def step_seven():
     root.after(10000, step_eight)
 
 def step_eight():
-#2.8.Światło zielone dla samochodu i światło czerwone dla pieszego gaśnie
+#2.8. Both the green light for cars and the red light for pedestrians turn off.
     TrafficSemaphor.itemconfig(green_traffic, fill=green[0])
     PedestrianSemaphor.itemconfig(red_pedestrian, fill=red[0])
-    # setting b_pedestrian state to clickable again
-    b_pedestrian.config(state="normal")
     global sequence_flag
     sequence_flag = False
+    global b_switch_state
+    # setting b_pedestrian state to clickable again as the sequence ends. If block is necessary to block enabling in night mode.
+    if b_switch_state == "day":
+        b_pedestrian.config(state="normal")
+
 
 b_switch = tkinter.Button(text="LIGHTS MODE:\n DAY\n\n PRESS TO SWITCH\nTO NIGHT MODE", font=30, fg='white', background='green', height=10, width=20, command=switch_mode)
 b_switch.place(x=600, y=50)
@@ -108,7 +111,7 @@ b_pedestrian.place(x=600, y=250)
 #sequence flag checks if the daylight sequence is being executed
 sequence_flag = False
 def night_lights():
-    global b_switch_state, sequence_flag, TrafficSemaphor
+    global b_switch_state, sequence_flag
     if b_switch_state == 'night':
         if sequence_flag == False:
             TrafficSemaphor.itemconfig(yellow_traffic, fill=yellow[1])
